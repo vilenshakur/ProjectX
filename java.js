4d510ad31366c385d86829f6e2a3e1be1d60a55d
vilenshakur <125366281+vilenshakur@users.noreply.github.com>
2024-10-17 20:32:37 +0500

    Update java.js

--- java.js
+++ java.js
@@ -17,9 +17,9 @@ document.addEventListener("DOMContentLoaded", function() {
     let roundCount = 0;
 
   const imagePairs = [
-        ['./img/Али1.jpg', './img/Тема1.jpg'],
-        ['./img/Дамиль3.jpg', './img/я2.jpg'],
-        ['./img/негр1.jpg', './img/Тема2.jpg'],
+        ['./Али1.jpg', './Тема1.jpg'],
+        ['./Дамиль3.jpg', './я2.jpg'],
+        ['./негр1.jpg', './Тема2.jpg'],
     ];
 
     const votes = [0, 0, 0, 0, 0, 0]; // Счетчики голосов для каждого человека
